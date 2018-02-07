6b3e271cb232f8bc1ea71e2b459009d6c4bf57e2
Sayed Rizvi <sayednaweed.rizvi@lexisnexis.com>
2018-02-07 12:57:58 +0530

    Docker file added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-
-app.listen(8000, () => {
-	console.log("Node running on port 8000");
+app.listen(8081, () => {	
+	console.log("Node running on port 8081");
 });
 
+app.get("/", (req,res) => {
+	res.send("Hello World");
+})
